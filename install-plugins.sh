3fa4a4eca308073d8231f3f5e2982287cef2d4a7
Renan de Souza Polisciuc <ext.renan.souza@elgin.com.br>
2021-12-11 19:20:46 -0300

    Trocado repo plugins

--- install-plugins.sh
+++ install-plugins.sh
@@ -2,5 +2,5 @@
 
 while read plugin version; do
   echo "Installing '${plugin}' (${version})"
-  /venv/bin/pip install git+https://github.com/alerta/alerta-contrib.git@${version}#subdirectory=${plugin}
+  /venv/bin/pip install git+https://github.com/renanpolisciuc/alerta-contrib.git@${version}#subdirectory=${plugin}
 done </app/plugins.txt
